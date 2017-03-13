11eba183c4a600c561c068ff9807b05a6ca8d234
quark314 <quark314@users.noreply.github.com>
2017-03-12 22:17:18 -0500

    reorganization with sp blog and research page

---
+++ researchPage/lowitz.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN" dir="ltr">
+<head profile="http://gmpg.org/xfn/11">
+<title>Observational Cosmology - Amy Lowitz</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="imagetoolbar" content="no" />
+<link rel="stylesheet" href="../../styles/layout.css" type="text/css" />
+<!-- Homepage Specific Elements -->
+<script type="text/javascript" src="scripts/jquery-1.4.1.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.7.2.custom.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.tabs.setup.js"></script>
+<!-- End Homepage Specific Elements -->
+</head>
+<body id="lowitz">
+
+<?php include_once("../../obsCosNavBar.php"); ?>
+
+
+<!-- ####################################################################################################### -->
+<div class="wrapper row3">
+  <div class="rnd">
+    <div id="container" class="clear">
+      <!-- ####################################################################################################### -->
+      
+	  <img class="imgr" src="lowitz.jpg" alt="lowitz" width="250" />
+	  <h1> Amy Lowitz </h1>
+	  <b>PhD Candidate</b><br>
+	  lowitz AT wisc.edu<br><br>
+	  
+	  Rm. 6246<br>
+	  Department of Physics <br>
+	  Chamberlin Hall<br>
+	  1150 University Ave<br>
+	  Madison, WI 53706-1390<br>
+	  Phone# (608) 262-5916 <br> <br> <br>
+    
+    <h1></h1>
+    <h1><a href="http://weblogarithms.com"> South Pole Blog! </a></h1><br><br><br>
+	  
+	  <h2>Education</h2>
+	  <ul>
+      UW - Madison, Physics PhD, 2010-present (expected defence early 2016)<br>
+      Brown University, Physics ScB, 2005-2009
+      <br><br>
+    </ul>
+    
+    <h2>Curriculum Vitae</h2>
+	    <ul>
+		  <li><a href="CV.pdf">PDF</a>  (last updated 8/2015)</li>
+      <br>
+		</ul>
+	  
+	  <h2>Current Research</h2>
+	  <ul>
+	    I am interested in Kinetic Inductance Detectors (KIDs) as candidate 
+			detectors for future Cosmic Microwave Background telescopes, including 
+			<a href="http://arxiv.org/pdf/1010.0645.pdf">QUBIC</a>.  My work focuses 
+      on pushing lumped element KIDs to the lowest possible optical frequencies.  
+      I design KIDs using EM simulation software and semi-analytic calculations, fabricate prototype arrays 
+      in the Detector Development Laboratory cleanroom facility at NASA Goddard, and test 
+      the arrays in our ADR-cooled laboratory cryostat with our novel 100 GHz window-free, 
+      vacuum waveguide feedthrough-free optical coupling scheme.  The goal is to demonstrate 
+      the sensitivity achievable with simple quasi-lumped element direct-absorbing KIDs at 100 GHz optical frequency.  
+      <br> <br>      
+		
+	  </ul>
+		
+	   <h2>Experience</h2>
+		<ul>
+		  <li>MIT Lincoln Lab, Radar Engineer, 2009-2010</li>
+        &emsp; - Machine learning, algorithm development
+		  <li>Brown University, Dell'Antonio Lab, Senior Thesis, 2008-2009</li>
+        &emsp; - Photometric redshifts, weak lensing
+		  <li>Princeton University, Gravity Group, Summer Research Student, 2007 & 2008 </li>
+        &emsp; - Cryogenic refrigeration, cryogenic electronics
+        <br><br>
+	  </ul>
+    
+    <h2>Publications</h2>
+    <ul>
+      A. E. Lowitz, A. D. Brown, and T. R. Stevenson, P. T. Timbie, and E. J. Wollack, "Design, fabrication, and testing of a TiN/Ti/TiN trilayer KID array for 3 mm CMB observations."
+      <em>Proceedings of the 16th International Workshop on Low Temperature Detectors</em>, Grenoble, FR, July 2015. To be published in a special issue of <em>JLTP</em>. (in preparation)
+      <br><br>
+      A. E. Lowitz, A. D. Brown, and T. R. Stevenson, P. T. Timbie, and E. J. Wollack, <a href="../../KIDs/SPIEpaper2014.pdf">"Design, fabrication, and testing of lumped element kinetic inductance detectors for 3 mm 
+      CMB Observations</a>," Proc. SPIE 9153, Millimeter, Submillimeter, and Far-Infrared Detectors and Instrumentation for Astronomy VII, 91532R (July 23, 2014); 
+      doi:10.1117/12.2057102.
+      <br><br>
+      A. E. Lowitz, E. M. Barrentine, S. R. Golwala, and P. T. Timbie, <a href="../../KIDs/LTDpaper2013.pdf">"A Comparison of Fundamental Noise in Kinetic Indictance 
+      Detectors and Transition Edge Sensors for Millimeter-wave Applications,"</a> <em>Proceedings of the 15th International Workshop on Low Temperature Detectors</em>, 
+      Pasadena, CA, June 2013. To be published in a special issue of <em>JLTP</em> (2014). DOI 10.1007/s10909-014-1133-5.  
+      <a href="http://arxiv.org/abs/1402.3601">arXiv1403.3601</a>. 
+      <br><br>
+      A. Ghribi, J. Aumont, E. S. Battistelli, A. Bau, L. Berge, J-Ph. Bernard, M. Bersanelli, M-A. Bigot-Sazy, G. Bordier, 
+      E. T. Bunn, F. Cavaliere, P. Chanial, A. Coppolecchia, T. Decourcelle, P. De Bernardis, M. De Petris, A-A. Drilien, L. Dumoulin, M. C. Falvella, 
+      A. Gault, M. Gervasi, M. Giard, M. Gradziel, L. Grandsire, D. Gayer, J-Ch. Hamilton, V. Haynes, Y. Giraud-Héraud, N. Holtzer, J. Kaplan, A. Korotkov, J. Lande, 
+      A. Lowitz, B. Maffei, S. Marnieros, J. Martino, S. Masi, A. Mennella, L. Montier, A. Murphy, M. W. Ng, E. Olivieri, F. Pajot, A. Passerini, F. Piacentini, M. Piat, 
+      L. Piccirillo, G. Pisano, D. Prêle, D. Rambaud, O. Rigaut, C. Rosset, M. Salatino, A. Schillaci, S. Scully, C. O'Sullivan, A. Tartari, P. Timbie, G. Tucker, 
+      L. Vibert, F. Voisin, B. Watson, M. Zannoni, "Latest Progress on the QUBIC Instrument," <em>Proceedings of the 15th International Workshop on Low Temperature Detectors</em>, Pasadena, CA, June 2013.  
+      To be published in a special issue of <em>JLTP</em> (2014). <a href="http://arxiv.org/abs/1307.5701">arXiv1307.5701</a>.
+      <br><br>
+    </ul>
+    
+    <h2>Presentations</h2>
+    <ul>
+      <li>"Design, fabrication, and testing of lumped element kinetic inductance detectors for 3 mm CMB Observations," SPIE Astronomical Telescopes and Instrumentation, Montreal, QC, Canada, 25 June 2014. (poster)</li>
+      <li>"The Cosmic Microwave Background," IceCube High School Internship Program, Madison, WI, 20 February, 2014 (public lecture)</li>
+      <li>"The Cosmic Microwave Background," Madison Astronomical Society, Madison, WI, 10 January, 2014. (public lecture)</li>
+      <li>"A Comparison of Fundamental Noise in Kinetic Inductance Detectors and Transition Edge Sensors for Millimeter-wave Applications," 15th International Workshop on Low Temperature Detectors, Pasadena, CA, 25 June 2013.  (poster)</li>
+      <li>"A Comparison of Fundamental Noise Limits in TESs and MKIDs," Keck Institute for Space Studies, 2nd Superconducting Nitride Detector Workshop.  Pasadena, CA, 21 February 2012. (talk)</li>
+    </ul>
+    
+    </ul>
+		
+
+		
+      
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="copyright" class="clear">
+    <p class="fl_left">
+	  Last Updated 11/2014 | <a href="http://www.obscos.pbworks.com/w/page/53150881/FrontPage">Internal Wiki</a>
+	</p>
+    <p class="fl_right">Template by <a href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+  </div>
+</div>
+</body>
+</html>
